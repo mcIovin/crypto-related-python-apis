01b3236b36c6bcd975a6f71332bde93a4f3ed0e5
me <60447858+ATI00@users.noreply.github.com>
2022-08-28 23:56:27 -0500

    Added to the selenium-opensea class the ability to refresh the OpenSea metadata for one or more tokens.

--- class_selenium_opensea.py
+++ class_selenium_opensea.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import pandas as pd
+from time import sleep
 from urllib.parse import urlunsplit, urlencode
 from typing import Union
 from selenium import webdriver
@@ -13,14 +14,18 @@ from helperclass_percent_tracker import PercentTracker
 
 
 class SeleniumOnOpensea:
-    """This class assists with 'browsing' OpenSea using Seleium."""
+    """
+        This class assists with 'browsing' OpenSea using Seleium.
+        :param contract_address: The address of the smart contract of the NFTs.
+    """
 
     # URL components
     __website_scheme = 'https'
     __website_network_location = 'opensea.io'
 
-    def __init__(self):
+    def __init__(self, contract_address: str):
         """Initialize an object for RV website Authentication."""
+        self.contract_address = contract_address
         self.driver = webdriver.Firefox
     # ------------------------ END FUNCTION ------------------------ #
 
@@ -44,8 +49,8 @@ class SeleniumOnOpensea:
     # ------------------------ END FUNCTION ------------------------ #
 
     def get_many_nfts_properties(self,
-                                 contract_address: str,
                                  token_ids: Union[list, set, tuple, pd.Series],
+                                 network: str = 'ethereum',
                                  include_numeric_traits: bool = True,
                                  log_counter: bool = True) -> pd.DataFrame:
 
@@ -71,7 +76,7 @@ class SeleniumOnOpensea:
                                  f" counter is {counter}, and item ID is {item}")
                 token_processing_successful = True
                 try:
-                    list_results.append(self.get_an_nfts_properties(contract_address, item, include_numeric_traits))
+                    list_results.append(self.get_an_nfts_properties(item, network, include_numeric_traits))
                 except Exception as e:
                     logging.error(f"Unable to get metadata properties for token {item}. The exception was: "
                                   f"{repr(e)}")
@@ -85,8 +90,8 @@ class SeleniumOnOpensea:
 
                 percent_tracker.update_progress(counter,
                                                 show_time_remaining_estimate=True,
-                                                str_description_to_include_in_logging="Getting properties for a list of "
-                                                                                      "NFTs.")
+                                                str_description_to_include_in_logging="Getting properties for a "
+                                                                                      "list of NFTs.")
 
             # Reset the list (that the for loop uses) to only the remaining tokens that had errors,
             # so the method can try to get the dat for those tokens again.
@@ -109,16 +114,16 @@ class SeleniumOnOpensea:
     # ------------------------ END FUNCTION ------------------------ #
 
     def get_an_nfts_properties(self,
-                               contract_address: str,
                                token_id: str,
+                               network: str = 'ethereum',
                                include_numeric_traits: bool = True) -> Union[None, dict]:
-        dict_with_nft_properties = {}
-        dict_with_nft_properties['token_id'] = token_id
-        opensea_url = self.__construct_opensea_url_for_a_specific_nft(contract_address, token_id)
-        if self.__load_opensea_page(opensea_url) == False:
+        dict_with_nft_properties = {'token_id': token_id}
+        opensea_url = self.__construct_opensea_url_for_a_specific_nft(token_id, network)
+        if not self.__load_opensea_page(opensea_url):
             return
 
-        div_with_nft_data = self.__get_div_with_nft_summary()
+        # Get the <div> with the NFT summary, using the class name
+        div_with_nft_data = self.__get_div_by_class_name('item--summary')
         dict_with_nft_properties.update(self.__extract_nft_properties_from_summary_div(div_with_nft_data))
 
         if include_numeric_traits:
@@ -126,6 +131,55 @@ class SeleniumOnOpensea:
             dict_with_nft_properties.update(dict_nft_numeric_traits)
 
         return dict_with_nft_properties
+    # ------------------------ END FUNCTION ------------------------ #
+
+    def refresh_many_nfts(self,
+                          token_ids: Union[list, set, tuple, pd.Series, range],
+                          network: str = 'ethereum',
+                          log_counter: bool = True,
+                          wait_x_seconds_per_request: float = 0):
+
+        num_tokens = len(token_ids)
+        percent_tracker = PercentTracker(num_tokens,
+                                         int_output_every_x_percent=5)
+        counter = 0
+
+        for item in token_ids:
+            if log_counter:
+                counter += 1
+                logging.info(f"Refreshing the next NFT on OpenSea. Loop"
+                             f" counter is {counter}, and item ID is {item}")
+            try:
+                self.refresh_an_nft(item, network)
+            except Exception as e:
+                logging.error(f"Was unable to refresh the metadata on OpenSea for token #{item}. The exception was: "
+                              f"{repr(e)}")
+
+            if wait_x_seconds_per_request > 0:
+                sleep(wait_x_seconds_per_request)
+
+            percent_tracker.update_progress(counter,
+                                            show_time_remaining_estimate=True,
+                                            str_description_to_include_in_logging="Getting properties for a "
+                                                                                  "list of NFTs.")
+    # ------------------------ END FUNCTION ------------------------ #
+
+    def refresh_an_nft(self, token_id: str, network: str = 'ethereum'):
+        opensea_url = self.__construct_opensea_url_for_a_specific_nft(token_id, network)
+        if not self.__load_opensea_page(opensea_url):
+            return
+
+        # As of this writing, the 'refresh' button for each NFT is on the NFT's page in
+        # a DIV labelled with a class named 'item--collection-toolbar-wrapper
+        div_with_toolbar = self.__get_div_by_class_name('item--collection-toolbar-wrapper')
+        try:
+            # There are several button in the <div>; the refresh one should be the first one
+            list_of_button_web_elements = div_with_toolbar.find_elements(By.TAG_NAME, value="button")
+            refresh_button = list_of_button_web_elements[0]
+            refresh_button.click()
+        except Exception as e:
+            logging.warning(f"Issue while refreshing the OpenSea metadata for tokenId {token_id}. The"
+                            f"exception was: {repr(e)}")
     # ------------------------ END FUNCTION ------------------------ #
 
     def close_driver(self):
@@ -136,20 +190,19 @@ class SeleniumOnOpensea:
         logging.info('Closed browser.')
     # ------------------------ END FUNCTION ------------------------ #
 
-    def __construct_opensea_url_for_a_specific_nft(
-            self,
-            contract_address: str,
-            token_id: str) -> str:
+    def __construct_opensea_url_for_a_specific_nft(self, token_id: str, network: str) -> str:
 
-        url_path = f"/assets/{contract_address}/{token_id}"
+        url_path = f"/assets/{network}/{self.contract_address}/{token_id}"
         dict_query_params = {}
+        network_location = f"testnets.{self.__website_network_location}" if \
+            (network == 'rinkeby' or network == 'goerli') else self.__website_network_location
 
         # the urlunsplit method expects five arguments. Below, they are self-explanatory, except for the
         # fifth argument. In the fifth argument, a 'fragment' of a URL (I believe it is a portion that goes after
         # a '#' hash symbol) can be passed. We don't need that, so we just pass the empty string. I'm not sure
         # why a double parentheses is needed to call the function.
         return urlunsplit((self.__website_scheme,
-                           self.__website_network_location,
+                           network_location,
                            url_path,
                            urlencode(dict_query_params),
                            ""))
@@ -158,7 +211,7 @@ class SeleniumOnOpensea:
     def __load_opensea_page(self, opensea_url: str) -> bool:
         successfully_loaded = True
         try:
-            self.driver.get(opensea_url)
+            self.driver.get(url=opensea_url)
         except Exception as e:
             logging.warning(f"Problem while opening: {opensea_url}. The Exception was: {repr(e)}")
             successfully_loaded = False
@@ -166,24 +219,25 @@ class SeleniumOnOpensea:
         return successfully_loaded
     # ------------------------ END FUNCTION ------------------------ #
 
-    def __get_div_with_nft_summary(self) -> Union[WebElement, None]:
+    def __get_div_by_class_name(self, class_name: str) -> Union[WebElement, None]:
         """
         This function expects a webpage with information for a specific NFT to already be
         loaded by the selenium driver.
+        :param class_name: The name of the class of the div being looked for.
         :return: A representation of the <div> html element that contains the
           NFT's summary information.
         """
 
-        div_with_nft_data_summary = WebElement
+        the_div_being_looked_for = WebElement
         try:
-            div_with_nft_data_summary = WebDriverWait(self.driver, timeout=30). \
-                until(EC.presence_of_element_located((By.CLASS_NAME, "item--summary")))
+            the_div_being_looked_for = WebDriverWait(self.driver, timeout=30). \
+                until(EC.presence_of_element_located((By.CLASS_NAME, class_name)))
         except Exception as e:
-            logging.warning(f"Problem while accessing the 'div' containing NFT properties. "
-                            f"The Exception was: {repr(e)}")
+            logging.warning(f"Problem while accessing the 'div' being looked for based on "
+                            f"the class name: {class_name}. The Exception was: {repr(e)}")
             return
 
-        return div_with_nft_data_summary
+        return the_div_being_looked_for
     # ------------------------ END FUNCTION ------------------------ #
 
     def __extract_nft_properties_from_summary_div(self,
